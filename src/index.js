a812f9e0fc02464ccd821720af4750d29870c557
Vicwin13 <vnwimo13@gmail.com>
2025-06-26 13:10:54 +0100

    recent update, realance website addition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { defaultProps, targetElements } from "./data/scrollRevealConfig";
+
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
